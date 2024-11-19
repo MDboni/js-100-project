8abd44d9a9b6d978ef0e8adac72c61f22f6c7141
boni <aminboni048@gmail.com>
2024-11-19 18:12:29 +0600

    3rd

--- js/boundel.js
+++ js/boundel.js
@@ -1,4 +1,29 @@
-const Counter = document.getElementById("counter");
+const Numounter = document.getElementById("counter");
 const Plusbtn = document.getElementById('plusbtn');
 const Minusbtn = document.getElementById('minusbtn');
 
+
+let counter = 0 ;
+
+Plusbtn.addEventListener('click', () =>{
+    
+  counter=counter+1;
+  Numounter.textContent = counter; 
+  
+  if(counter >= 10){
+    Plusbtn.setAttribute('disabled',true);
+  }
+  else{
+    Plusbtn.
+  }
+    
+    
+})
+
+Minusbtn.addEventListener('click', () =>{
+    counter=counter-1;
+    Numounter.textContent = counter;
+
+    
+})
+ 
